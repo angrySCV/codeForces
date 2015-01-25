cfc3ab9b9cd9c76bcfbfe5a9983b3f02894f46b1
Dima Kubitskiy <dima@bepromo.ru>
2015-01-26 00:48:29 +0300

    add task

--- archive/unsorted/2015.01/2015.01.23 - Codeforces Round #287 (Div. 2)/TaskA.java
+++ archive/unsorted/2015.01/2015.01.23 - Codeforces Round #287 (Div. 2)/TaskA.java
@@ -1,31 +1,40 @@
 package angrySCV;
 
+
+
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class TaskA {
 	public void solve (int testNumber, Scanner in, PrintWriter out) {
 		int colichestvoInstrumentov = in.nextInt();
 		int colichestvoDney = in.nextInt();
-		int[] obuchenie = new int[colichestvoInstrumentov];
+		int[][] obuchenie = new int[colichestvoInstrumentov][2];
 		for (int i = 0; i < colichestvoInstrumentov; i++) {
-			obuchenie[i] = in.nextInt();
+			obuchenie[i][0] = in.nextInt();
+			obuchenie[i][1] = i;
 		}
 		int countInstrument = 0;
 		int countDay = 0;
 		int totalCount = 0;
-		Arrays.sort(obuchenie);
+		Arrays.sort(obuchenie, new Comparator<int[]>() {
+			@Override
+			public int compare (int[] o1, int[] o2) {
+				return Integer.compare(o1[0], o2[0]);
+			}
+		});
 out:
 		for (int i = 0; i < colichestvoInstrumentov; i++) {
-			if (countDay + obuchenie[i] <= colichestvoDney){
-				countDay = countDay+obuchenie[i];
+			if (countDay + obuchenie[i][0] <= colichestvoDney){
+				countDay = countDay+obuchenie[i][0];
 				countInstrument++;
 			} else break out;
 		}
 		System.out.println(countInstrument);
 		for (int i = 0; i < countInstrument; i++) {
-			System.out.print(obuchenie[i] + " ");
+			System.out.print(obuchenie[i][1]+1 + " ");
 		}
 	}
 }
